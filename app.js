174ec5546986cfd66027060c99b2545dfc0fe147
khanhngg <khanhnguye94@gmail.com>
2018-05-11 11:43:31 -0700

    Setup nodejs express app

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+
+// Create connection
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'csc675project'
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connect to db...');
+});
+
+const index = require('./routes/index');
+const teams = require('./routes/teams');
+const players = require('./routes/players');
+const coaches = require('./routes/coaches');
+const mascots = require('./routes/mascots');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use('/', index);
+app.use('/teams', teams);
+app.use('/players', players);
+app.use('/coaches', coaches);
+app.use('/mascots', mascots);
+
+app.listen('6750', () => {
+  console.log('Listening on port 6750...');
+});
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (request, response, next) => {
+  response.render('index', {
+    title: 'CSC 675 - Project'
+  });
+});
+
+module.exports = router;
